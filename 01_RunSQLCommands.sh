105f45d5e5c10f7355c9ea480e0a5443e4d0d65b
actuallyfro <actuallyfro@github.com>
2021-11-07 09:22:26 -0600

    {RunSQLCommands} pushing latest: it can be configured for create vs. insert -- checks for file

--- 01_RunSQLCommands.sh
+++ 01_RunSQLCommands.sh
@@ -5,17 +5,38 @@ if [ -z "$(which pv)" ]; then
   exit 1
 fi
 
-nameDB="test"
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <action>"
+  echo "  action: create | insert "
+  exit 1
+fi
+
+file="InsertTables.sql"
+
+#It's MORE likely to run insert more often than create
+# if [ "$1" == "insert" ]; then
+if [ "$1" == "create" ]; then
+  file="CreateTables.sql"
+fi
+
+# nameDB="test"
 
 # if [ ! -f ~/.my.cfg ]; then
 #   echo "[ERROR] ~/.my.cfg DOES NOT exist! Aborting..."
 #   exit 1
 # fi
 
+if [ ! -f "$file" ]; then
+  echo "[ERROR] $file DOES NOT exist! Aborting..."
+  exit 1
+fi
+
 # # WORKS
 # mysql --defaults-file=my.cfg -h localhost < CreateTables.sql
 
 # https://dba.stackexchange.com/questions/17367/how-can-i-monitor-the-progress-of-an-import-of-a-large-sql-file
 # pv file-name.sql | mysql -u root -pPass <DataBaseName>
-pv CreateTables.sql | mysql --defaults-file=my.cfg -h localhost
+pv "$file" | mysql --defaults-file=my.cfg -h localhost
+
 # pv CreateTables.sql | mysql --defaults-file=my.cfg -h localhost "db_"$nameDB
