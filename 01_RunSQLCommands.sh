3b50f394e242255a6a7f6f36ca23e21553d818c8
actuallyfro <actuallyfro@github.com>
2021-11-06 22:49:07 -0500

    {RunSQLCommands} .cfg is not loading... directly addressing the file

--- 01_RunSQLCommands.sh
+++ 01_RunSQLCommands.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [ ! -f ~/.my.cfg ]; then
-  echo "[ERROR] ~/.my.cfg DOES NOT exist! Aborting..."
-  exit 1
-fi
+# if [ ! -f ~/.my.cfg ]; then
+#   echo "[ERROR] ~/.my.cfg DOES NOT exist! Aborting..."
+#   exit 1
+# fi
 
-mysql -h localhost < CreateTables.sql
-
-#Alternatively: --defaults-extra-file=my.cfg
-
+mysql --defaults-file=my.cfg -h localhost < CreateTables.sql
