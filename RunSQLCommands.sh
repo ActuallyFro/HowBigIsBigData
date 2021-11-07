8141271532d08b0a9b0cc78e23b7147471e96308
actuallyfro <actuallyfro@github.com>
2021-11-06 20:34:39 -0500

    {RunSQLCommands} Setting up cfg running...

--- RunSQLCommands.sh
+++ RunSQLCommands.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-mysql -h localhost -u bigdata -p 123!@#qweQWE < CreateTables.sql
+
+if [ ! -f ~/.my.cfg ]; then
+  echo "[ERROR] ~/.my.cfg DOES NOT exist! Aborting..."
+  exit 1
+fi
+
+mysql -h localhost < CreateTables.sql
+
+#Alternatively: --defaults-extra-file=my.cfg
+
