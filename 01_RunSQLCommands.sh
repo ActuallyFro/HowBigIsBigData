2f8f7965c73c3005209b3598c420fcaf2cce80ef
actuallyfro <actuallyfro@github.com>
2021-11-07 12:03:15 -0600

    {RunSQLCommands.sh} Trying to prepare for the .sql to load the .csv file...

--- 01_RunSQLCommands.sh
+++ 01_RunSQLCommands.sh
@@ -12,20 +12,15 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-file="InsertTables.sql"
+# FOR UUIDS! https://remarkablemark.org/blog/2020/05/21/mysql-uuid-bin/
+
+file="InsertTables.sql" #Runs the .sql that LOADS generateRandFloat.csv
 
 #It's MORE likely to run insert more often than create
 # if [ "$1" == "insert" ]; then
 if [ "$1" == "create" ]; then
   file="CreateTables.sql"
 fi
-
-# nameDB="test"
-
-# if [ ! -f ~/.my.cfg ]; then
-#   echo "[ERROR] ~/.my.cfg DOES NOT exist! Aborting..."
-#   exit 1
-# fi
 
 if [ ! -f "$file" ]; then
   echo "[ERROR] $file DOES NOT exist! Aborting..."
@@ -38,5 +33,6 @@ fi
 # https://dba.stackexchange.com/questions/17367/how-can-i-monitor-the-progress-of-an-import-of-a-large-sql-file
 # pv file-name.sql | mysql -u root -pPass <DataBaseName>
 pv "$file" | mysql --defaults-file=my.cfg -h localhost
+#^---- likely DOES NOT work, because this is based on the INSERT vs. LOAD FILE concept
 
 # pv CreateTables.sql | mysql --defaults-file=my.cfg -h localhost "db_"$nameDB
